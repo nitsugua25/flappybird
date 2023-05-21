deebad9f298b2b77bd32a996b7e29778646013e2
augustin michiels <augustin.michiels@std.heh.be>
2023-05-21 17:46:10 +0200

    first commit on feature création du background boucle de jeu et boucle for event avec possibilité de quitté avec la croix

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 
 pygame.init()
+
+WIDTH = 280
+HEIGHT = 500
+
+surface = pygame.display.set_mode((WIDTH, HEIGHT))
+surface.blit(pygame.image.load("assets/images/background-day.png"), (0, 0))
+
+pygame.display.set_caption("flappy bird")
+
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	pygame.display.flip()
